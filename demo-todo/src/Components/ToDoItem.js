4d3a866c38ab3a9405e26680105e48cc53cffc7e
Dharshini S <dharshiniselvaraj12122003@gmail.com>
2024-06-03 20:50:29 +0530

    Day -3 commit

--- demo-todo/src/Components/ToDoItem.js
+++ demo-todo/src/Components/ToDoItem.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 function ToDoItem(){
-    const[task,setTask] = useState(["Eat Breakfast"])
+    {/*For already having some task*/}
+
+    const[task,setTask] = useState(["Eat Breakfast"]) 
+    {/*for adding new task*/}
     const[newTask,setNewTask]  = useState("")
+
     function addTask(){
+        {/*when input field is empty ,it will not go the condition and trim for cheking the empty spaces leaving the whiteSpaces*/}
         if(newTask.trim()!== ""){
+            {/*for adding*/}
             setTask(t=>[...t, newTask])
-            setNewTask("")
+
+            {/*once after ading the task making the input field to be empty*/}
+            setNewTask(" ")
         } 
     }
     function DeleteTask(index){
@@ -16,10 +24,11 @@ function ToDoItem(){
         <div >
             <h1>ToDo List</h1>
             <div>
-                <input type='text' placeholder='Enter a task...' value={newTask} onChange={(event) => setNewTask(event.target.value)}/>
+                <input type='text' placeholder='Enter a task...' value={newTask} onChange={(event) => setNewTask(event.target.value)}/>{/*Getting input*/}
                 <button onClick={addTask}>Add</button>
             </div>
             <ol>
+                {/*task => for getting the current element , index => for getting the current index value and add it to list*/}
                 {task.map((task, index) =>
                    <li key={index}>
                         <span>{task}</span>
